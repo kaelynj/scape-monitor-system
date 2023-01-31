26056fcdd5b350fbdfcc1d2575aa71ac3dc72e5e
kaelynj <kaelyn.ferris@gmail.com>
2023-01-31 09:51:56 -0500

    Minimally working gdrive upload
    Asks for OAuth .json file and generates a token to remain valid.  Uploads test-scape-imgage.jpg to google drive.

--- gdrive_quickstart.py
+++ gdrive_quickstart.py
@@ -8,32 +8,38 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+from googleapiclient.http import MediaFileUpload
 
+
+# If modifying these scopes, delete the file token.json.
+#SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.metadata']
+
+def get_credentials():
+    ''' Obtain user credentials.  First by checking if the auth token file
+    exists, ask the user to login and validate the credentials if it doesn't
+    '''
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, have the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            cred_secrets_file = input("Filename of client_secret: ")
+            flow = InstalledAppFlow.from_client_secrets_file(cred_secrets_file, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save teh credentials for the next run
+        with open('token.json', 'w') as tokenFile:
+            tokenFile.write(creds.to_json())
+    return creds
 
 def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
+    creds = get_credentials()
     try:
         service = build('drive', 'v3', credentials=creds)
 
@@ -53,5 +59,44 @@ def main():
         print(f'An error occurred: {error}')
 
 
+def uploadTest():
+    """Upload test image to a specified folder
+    """
+    creds = get_credentials()
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        #Call the drive v3 API
+        scapeFolder = service.files().list(q="name = 'ScapePhotos'", #q="mimeType = 'application/vnd.google-apps.folder'", # will query all folders q="name = 'ScapePhotos'"
+                                        spaces='drive',
+                                        fields='nextPageToken, '
+                                                'files(id, name)').execute()
+
+        # Make this a less bespoke approach, add in modularity
+        folders = scapeFolder.get('files', [])
+        folder_id = folders[0]['id']
+        file_metadata = {'name':'test-scape-image.jpg',
+                        'parents': [folder_id]}
+        media = MediaFileUpload('test-scape-image.jpg',
+                                        mimetype='image/jpeg',
+                                        resumable=True)
+        file = service.files().create(body=file_metadata, media_body=media,
+                                        fields='id').execute()
+        print("File ID: {} ".format(file.get("id")))
+        return file.get('id')
+
+        if not results:
+            print('Nothing found...')
+            return
+        #folders = results.get('files', [])
+        #print(folders)
+        for item in folders:
+            print('{} ({})'.format(item['name'], item['id']))
+        print("############################################################")
+    except HttpError as error:
+        #You should handle errors here in case of no connection
+        print('An error occured: {}'.format(error))
+
 if __name__ == '__main__':
-    main()
+    #main()
+    uploadTest()
